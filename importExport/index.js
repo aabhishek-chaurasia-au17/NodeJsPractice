0302615fba04bed3b3ef78140ca4af1ed25941bc
Abhishek <abhishekc838@gmail.com>
2021-09-28 22:10:11 +0530

    Node learn

---
+++ importExport/index.js
@@ -0,0 +1,6 @@
+const {add, sub, Name} = require('./opretor')
+
+console.log(add(5, 15)); 
+console.log(sub(15 ,5)); 
+
+console.log(Name);
---
+++ importExport/opretor.js
@@ -0,0 +1,10 @@
+
+const add = (a, b) => a+b
+
+const sub = (a, b) => a-b
+
+let Name = 'Abhishek chaurasia'
+
+module.exports.add = add;
+module.exports.sub = sub;
+module.exports = {add, sub, Name}
